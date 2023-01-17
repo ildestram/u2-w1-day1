81fb5ee929f95cf5f31ea149975e50b96b3ce490
Ilde Stramandinoli <109019707+ildestram@users.noreply.github.com>
2023-01-17 13:35:31 +0100

    finito fino al 14

--- d3-u2.js
+++ d3-u2.js
@@ -121,6 +121,113 @@ oddOrEven(14);
       console.log("Uguale a 10 o maggiore");
     }
 */
+// let val = 7;
+let val = 7;
+switch (val) {
+    case val < 10:
+        console.log("meno di 10");
+        break;
+    case val < 5:
+        console.log("meno di 5");
+        break;
+    case val >= 10:
+        console.log("uguale a 10 o maggiore");
+    default:
+        console.log("hai finito i numeri");
+};
+
+/*
+ESERCIZIO 11
+  Crea un blocco condizionale if/else annidato su più livelli per mostrare in console il messaggio corretto in ogni condizione.
+
+  num < 5 - mostra in console "Tiny"
+  num < 10 - mostra in console "Small"
+  num < 15 - mostra in console "Medium"
+  num < 20 - mostra in console "Large"
+  num >= 20 - mostra in console "Huge"
+*/
+
+let pippo;
+// switch(pippo){
+//     case pippo < 5:
+//         console.log("tiny");
+//         break;
+//     case pippo < 10:
+//         console.log("small");
+//         break;
+//     case pippo < 15:
+//         console.log("medium");
+//         break;
+//     case pippo < 20:
+//         console.log("large");
+//         break;
+//     case pippo >= 20:
+//         console.log("huge");
+//         break;
+//     default:
+//         console.log("hai finito");
+// };
+function numeroPippo(pippo){
+    if(pippo < 5){
+        console.log("tiny");
+    } if(pippo < 10){
+        console.log("small");
+    } if(pippo < 15){
+        console.log("medium");
+    } if(pippo < 20){
+        console.log("large");
+    } if(pippo >= 20){
+        console.log("huge");
+    };
+};
+numeroPippo();
+
+/*  ESERCIZIO 12
+  Usa un operatore ternaio per assegnare ad una variabile chiamata "gender" i valori "male" o "female".
+  La scelta deve essere basata sul valore di un'altra variabile chiamata isMale.
+*/
+
+let isMale;
+let gender = isMale ? "female" : "male";
+// isMale è la condizione
+console.log(gender);
+
+/* ESERCIZIO 13
+  Mostra i numeri da 0 a 5 (incluso) in ordine ascendente utilizzando un ciclo while.
+*/
+let n = 0;
+x = 0;
+while(n < 5){
+    n++;
+    console.log(n);
+    x += n;
+};
+
+/* ESERCIZIO 14
+  Mostra i numeri da 0 a 10 (incluso) in ordine ascendente utilizzando un ciclo for.
+*/
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+};
+
+/* ESERCIZIO 15
+  Mostra i numeri da 0 a 10 (incluso) in ordine ascendente, ma evitando di mostrare i numeri 3 e 8.
+*/
+
+/* ESERCIZIO 16
+  Scrivi un ciclo in JavaScript per iterare da 0 a 15. Per ciascun elemento, il ciclo deve controllare che il valore corrente sia pari o dispari, e mostrare il risultato in console.
+*/
+
+/* ESERCIZIO 17
+  Scrivi un algoritmo che iteri i numeri da 1 a 100, stampandoli in console. Se un valore tuttavia è multiplo di 3, stampa al suo posto la parola "Fizz" e se il numero è multiplo di 5, stampa "Buzz". Se le condizioni si verificano entrambe, stampa "FizzBuzz".
+*/
+
+
+/* ESERCIZIO 18
+  Scrivi un algoritmo per controllare il giorno della settimana. Usa uno switch-case sulla variabile "day", che può avere un valore da 1 a 7.
+  Ad esempio, se il valore di "day" è 1, stampa in console "Lunedì"; se il valore fosse 3, in console dovrebbe comparire "Mercoledì".
+*/
+
 
 
 
